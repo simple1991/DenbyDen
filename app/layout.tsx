ea2bcbeada8528e2fc72e6b8994b8f2b40c322d8
张乃健 <18522415626@163.com>
2025-11-21 14:16:39 +0800

    手机端缩放优化

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
+  initialScale: 1.0,
+  maximumScale: 1.0,
   userScalable: false,
 }
 
